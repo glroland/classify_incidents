b48e48223cb5887c8f0899d2fca833e225f8b39d
Lee Roland <glroland@hotmail.com>
2025-10-19 11:46:06 -0400

    fixed subcategories

--- src/commands/from_space.py
+++ src/commands/from_space.py
@@ -1,11 +1,14 @@
 """ Web Command for Importing Incidents from an Object Store. """
 import os
 import logging
+import json
 from pathlib import Path
 from pydantic import BaseModel, ConfigDict
 import pandas as pd
 from commands.from_string import FromStringCommand
+from gateways.inference_gateway import InferenceGateway
 from gateways.object_storage_gateway import ObjectStorageGateway
+from utils.get_prompt import prompts
 
 logger = logging.getLogger(__name__)
 
@@ -14,6 +17,12 @@ class FromSpaceCommand(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     # constants
+    COLUMN_AI_CATEGORY : str = "Category"
+    COLUMN_AI_SUBCATEGORY : str = "Subcategory"
+    UNKNOWN_CATEGORY : str = "Unspecified by AI"
+
+    # config
+    MAX_RETRIES : int = 5
     files_to_skip : list[str] = []
 
     # input parameters
@@ -95,8 +104,109 @@ class FromSpaceCommand(BaseModel):
                                        command.status
                                        ]
 
+        # Augment data frame with new column for category
+        df = self.generalize_subcategories(df)
+
         self.analysis = df
 
         # save analysis as a CSV
         analysis_csv = df.to_csv(index=False)
         gateway.upload(f"{self.space_id}/analysis.csv", analysis_csv)
+
+
+    def generalize_subcategories(self, df):
+        """ Takes a dataframe with a subcategory column and uses AI to generalize
+            the column list into a shorter list of parent categories.
+            
+            df - data frame with subcategory column
+        """
+        logger.info("Generalizing Subcategories")
+
+        # handle condition where there are no subcategories
+        subcategories = df[self.COLUMN_AI_SUBCATEGORY].unique()
+        if subcategories is None or len(subcategories) == 0:
+            logger.error("No subcategories exist in data frame suggesting invalid AI output.")
+            return df
+
+        # create array for unique subcategories
+        subcategories_csv = ""
+        for subcategory in subcategories:
+            if subcategory is not None and len(subcategory) > 0:
+                if len(subcategories_csv) > 0:
+                    subcategories_csv += ","
+                subcategories_csv += subcategory
+        logger.debug("Subcategories CSV...   '%s'", subcategories_csv)
+
+        # setup inference gateway
+        gateway = InferenceGateway()
+
+        category_mappings = None
+
+        # convert subcategories into parent categories
+        retries = 0
+        while retries < self.MAX_RETRIES:
+            categories_json_str = None
+            try:
+                categories_json_str = gateway.simple_chat(prompts.ROLLUP_SUBCATEGORIES,
+                                                          subcategories_csv)
+                logger.debug("Categories from Subcategories Response == %s", categories_json_str)
+                category_mappings = json.loads(categories_json_str)
+
+                if category_mappings is not None:
+                    break
+            except Exception as e:  # pylint: disable=broad-exception-caught
+                logger.warning("An error occurred while trying to create generalized categories list.  Retrying...  Exception=%s.  JSON_String=%s", e, categories_json_str)   # pylint: disable=line-too-long
+                retries += 1
+        if category_mappings is None:
+            msg = "After several retries, the LLM was unable to produce a parsable list of JSON categories for processing.  Processing is failing..."   # pylint: disable=line-too-long
+            logger.error(msg)
+            raise ValueError(msg)
+
+        # update dataframe with new category column
+        new_column_category = []
+        for index, row in df.iterrows():        # pylint: disable=unused-variable
+            subcategory = row[self.COLUMN_AI_SUBCATEGORY]
+            category = self.get_category_for_subcategory(category_mappings, subcategory)
+            if category is None:
+                logger.warning("Category is unknown - nothing specified for subcategory:  %s",
+                               subcategory)
+                category = self.UNKNOWN_CATEGORY
+            new_column_category.append(category)
+        df[self.COLUMN_AI_CATEGORY] = new_column_category
+
+        # change order of columns
+        columns = df.columns.tolist()
+        columns.remove(self.COLUMN_AI_CATEGORY)
+        subcategory_index = columns.index(self.COLUMN_AI_SUBCATEGORY)
+        columns.insert(subcategory_index, self.COLUMN_AI_CATEGORY)
+        df = df[columns]
+
+        return df
+
+    def get_category_for_subcategory(self, category_mappings, subcategory):
+        """ Gets the category name for the provided subcategory.
+        
+            category_mappings - category mappings as returned by LLM
+            subcategory - subcategory to find
+        """
+        # handle empty subcategories
+        if subcategory is None or len(subcategory) == 0:
+            logger.warning("Empty subcategory...  Using default.")
+            return None
+
+        # loop through categories
+        for category_group in category_mappings:
+            if category_group is not None and \
+                "category" in category_group and \
+                "subcategories" in category_group:
+                # check for a match
+                category = category_group["category"]
+                for s in category_group["subcategories"]:
+                    if s == subcategory:
+                        return category
+            else:
+                logger.warning("Unexpected category structure.  Category Group=%s", category_group)
+
+        # no match
+        logger.warning("No match found for subcategory.  Subcategory=%s", subcategory)
+        return None
