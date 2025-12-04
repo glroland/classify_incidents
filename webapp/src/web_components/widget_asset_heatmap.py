24051d5bb6791cbf5f3e466c3034a1351016723d
Lee Roland <glroland@hotmail.com>
2025-12-03 21:38:35 -0500

    develoepd heat map UI widget

--- webapp/src/web_components/widget_asset_heatmap.py
+++ webapp/src/web_components/widget_asset_heatmap.py
@@ -1,5 +1,13 @@
+import logging
+from calendar import monthrange
 import streamlit as st
-import plotly.graph_objects as go
+import pandas as pd
+from web_components.widget_heatmap_grid import show_heatmap_grid_widget
+
+logger = logging.getLogger(__name__)
+
+MAX_ROWS = 20
+PIVOT_DATE_COLUMN = "Date_Reported"
 
 def show_asset_heatmap_widget(df):
     """ Display a heatmap that shows ticket counts by assets by day.
@@ -10,34 +18,91 @@ def show_asset_heatmap_widget(df):
     if not "Date_Reported" in df.columns:
         st.write("Unsupported Data Import version.  Skipping heatmap render.")
         return
+    
+    """
+    0   Incident_File      5 non-null      object
+    1   Row                5 non-null      int64 
+    2   Asset              5 non-null      object
+    3   Summary            5 non-null      object
+    4   Category           5 non-null      object
+    5   Subcategory        5 non-null      object
+    6   Is_Manual          5 non-null      bool  
+    7   Is_Outage          5 non-null      bool  
+    8   Status             5 non-null      object
+    9   Date_Reported      4 non-null      object
+    10  Date_Resolved      4 non-null      object
+    11  Count_by_Category  5 non-null      int64 
+    """
 
-    # prepare data
-    null_date_val = df["Date_Reported"].dropna().min()
-    df["Date_Reported"] = df["Date_Reported"].fillna(null_date_val)
+    # prepare dataframe for date analysis
+    df = df.dropna(subset=[PIVOT_DATE_COLUMN])
+    if len(df) == 0:
+        logger.error("Data Set only had empty / null date data.")
+        return
 
-    # build X and Y axises
-    asset_list = df["Asset"]
-    tdate_list = df["Date_Reported"]
+    # setup date range
+    max_date = pd.to_datetime(df[PIVOT_DATE_COLUMN]).max()
+    print ("HELO")
+    print (type(max_date))
+    print (max_date)
+    month = max_date.month
+    year = max_date.year
+    days_in_month = monthrange(year, month)[1]
 
-    # populate data per cell
-    counts_dim = []
-    for asset in asset_list:
-        cell_list = []
-        for tdate in tdate_list:
-            count = ((df['Asset'] == asset) & (df['Date_Reported'] == tdate)).sum()
-            cell_list.append(count)
-        counts_dim.append(cell_list)
+    # prepare core dataframe (Asset/Date/Count)
+    df['Date'] = pd.to_datetime(df[PIVOT_DATE_COLUMN]).dt.normalize()
+    df = df[(df['Date'] >= f"{year}-{month}-1") & (df['Date'] <= f"{year}-{month}-{days_in_month}")]
+    df = df[['Asset', 'Date']]
+    df = df.groupby(['Asset', 'Date']).size().reset_index(name='Count')
+    logging.info("Refined Data Frame: %s", df.info())
+
+    # append total count (Asset/Count)
+    df['Total'] = df.groupby('Asset')['Count'].transform('sum')
+
+    # sort data frame
+    df = df.sort_values(by=['Total', 'Asset', 'Date'], ascending=[False, True, True])
+
+    # build header
+    header = ["Config Item"]
+    for i in range(days_in_month):
+        header.append(str(i + 1))
+    header.append("Count")
+
+    # build asset rows
+    rows = []
+    prior_row = None
+    for index, row in df.iterrows():
+        # collect data points
+        asset = row["Asset"]
+        date = row["Date"]
+        date_count = row["Count"]
+        total = row["Total"]
+
+        # create new row?
+        if prior_row is not None and prior_row[0] != asset:
+            prior_row = None
+
+        # build new row
+        if prior_row is None:
+            # have we hit the max row count?
+            if len(rows) >= MAX_ROWS:
+                break
+
+            # create new row
+            asset_row = [asset]
+            for i in range(days_in_month):
+                cell_value = None
+                if date.day == i:
+                    cell_value = date_count
+                asset_row.append(cell_value)
+            asset_row.append(total)
+
+            rows.append(asset_row)
+            prior_row = asset_row
+        
+        # update existing
+        else:
+            prior_row[date.day + 1] = date_count
 
     # display heatmap
-    custom_colorscale = [
-        [0.0, 'white'],     # 0% of the data range maps to blue
-        [0.5, 'yellow'],    # 50% of the data range maps to yellow
-        [1.0, 'red']        # 100% of the data range maps to red
-    ]
-    fig = go.Figure(data=go.Heatmap(
-            z=counts_dim,
-            x=tdate_list,
-            y=asset_list,
-            colorscale=custom_colorscale,
-            hoverongaps=True))
-    st.plotly_chart(fig, key="Asset", on_select="rerun")
+    show_heatmap_grid_widget(f"Top {MAX_ROWS} Incident Heat Map", year, month, header, rows)
---
+++ webapp/src/web_components/widget_heatmap_grid.py
@@ -0,0 +1,120 @@
+from calendar import month_name
+import streamlit as st
+import streamlit.components.v1 as components
+from pandas import DataFrame
+
+def show_heatmap_grid_widget(title, year, month, header, rows,
+                             threshold_low=1, threshold_medium=2, threshold_high=3):
+    """ Renders a heat map grid
+    
+        header - header columns
+        rows - list of rows
+    """
+
+    st.header(f"{title} ({month_name[month]} {year})")
+
+    html = """
+    <style>
+        .gridComponent {
+            font-family: "Verdana", sans-serif;
+            font-size: 12px;
+
+            align: center;
+        }
+        .gridComponent table {
+            width: 100%;
+            border-collapse: collapse;
+            align: center;
+        }
+        .gridComponent tr td {
+            border-width: 1px;
+            border-style: solid;
+            border-color: black;
+            padding: 0;
+            margin: 0;
+            border-spacing: 0;
+            text-align: center;
+        }
+        .gridComponent td {
+            width: 25px;
+        }
+        .gridStrong {
+            font-weight: bold;
+        }
+        .gridComponent thead {
+            background-color: black;
+            color: white;
+        }
+        .gridCellHigh {
+            background-color: red;
+        }
+        .gridCellMedium {
+            background-color: yellow;
+        }
+        .gridCellLow {
+            background-color: green;
+        }
+    </style>
+    <div id="grid" class="gridComponent">
+        <table>
+    """
+    
+    # display header
+    if header is not None and len(header) > 0:
+        html += """
+        <thead>
+            <tr>
+        """
+        for h in header:
+            html += f"<td>{h}</td>"
+        html += """
+            </tr>
+        </thead>
+        """
+
+    # display rows
+    if rows is not None and len(rows) > 0:
+        html += """
+        <tbody>
+        """
+        for r in rows:
+            html += "<tr>"
+            for i in range(len(r)):
+                val_str = ""
+                val_class = ""
+                val = r[i]
+
+                # trailing column
+                if i == (len(r) - 1):
+                    val_class = " class=\"gridStrong\""
+                    if val is not None:
+                        val_str = val
+
+                # number
+                elif val is not None and (type(val) == int or \
+                    (type(val) == str and val.isdigit())):
+                    val_str = val
+                    if int(val) >= threshold_high:
+                        val_class = " class=\"gridCellHigh\""
+                    elif int(val) >= threshold_medium:
+                        val_class = " class=\"gridCellMedium\""
+                    elif int(val) >= threshold_low:
+                        val_class = " class=\"gridCellLow\""
+            
+                # string
+                elif val is not None:
+                    val_str = val
+
+                html += f"<td{val_class}>{val_str}</td>"
+            html += "</tr>"
+        html += """
+        </tbody>
+        """
+    
+    # close out html
+    html += """
+        </table>
+    </div>
+    """
+    
+    components.html(html, height=600)
