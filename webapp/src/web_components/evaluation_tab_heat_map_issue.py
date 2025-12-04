f9aa068b082bbd584bff11a205426d5120f84115
Lee Roland <glroland@hotmail.com>
2025-12-03 22:09:02 -0500

    implemented category heat map

--- webapp/src/web_components/evaluation_tab_heat_map_issue.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def view_evaluation_heat_map_by_issue(space_id, command):
-    st.header("View Heat Map - Categorized by Issue")
--- webapp/src/web_components/evaluation_tab_heat_maps.py
+++ webapp/src/web_components/evaluation_tab_heat_maps.py
@@ -9,32 +9,26 @@ logger = logging.getLogger(__name__)
 MAX_ROWS = 20
 PIVOT_DATE_COLUMN = "Date_Reported"
 
-def view_evaluation_heat_map_by_server(space_id, command):
-    # retrieve data set
-    df = command.analysis_df
-    if df is None or len(df) == 0:
-        st.write("Please run the analysis tool first.")
-        return
+"""
+0   Incident_File      5 non-null      object
+1   Row                5 non-null      int64 
+2   Asset              5 non-null      object
+3   Summary            5 non-null      object
+4   Category           5 non-null      object
+5   Subcategory        5 non-null      object
+6   Is_Manual          5 non-null      bool  
+7   Is_Outage          5 non-null      bool  
+8   Status             5 non-null      object
+9   Date_Reported      4 non-null      object
+10  Date_Resolved      4 non-null      object
+11  Count_by_Category  5 non-null      int64 
+"""
 
+def render_heat_map(title, pivot_entity_type, pivot_column, df):
     # validate that key columns exist before trying to render
     if not PIVOT_DATE_COLUMN in df.columns:
         st.write("Unsupported Data Import version.  Skipping heatmap render.")
         return
-    
-    """
-    0   Incident_File      5 non-null      object
-    1   Row                5 non-null      int64 
-    2   Asset              5 non-null      object
-    3   Summary            5 non-null      object
-    4   Category           5 non-null      object
-    5   Subcategory        5 non-null      object
-    6   Is_Manual          5 non-null      bool  
-    7   Is_Outage          5 non-null      bool  
-    8   Status             5 non-null      object
-    9   Date_Reported      4 non-null      object
-    10  Date_Resolved      4 non-null      object
-    11  Count_by_Category  5 non-null      int64 
-    """
 
     # prepare dataframe for date analysis
     prior_count = len(df)
@@ -45,7 +39,7 @@ def view_evaluation_heat_map_by_server(space_id, command):
         st.markdown(f":red[**{msg}**]")
     if len(df) == 0:
         logger.warning("Data Set has no data to process in heat map view")
-        st.markdown("I'm sorry but this data set has no data to analyze.  This may be due to the analysis process not running first or an issue with the AI model analyzing the raw data.")
+        st.markdown("I'm sorry but this data set has no data to analyze.\n\nThis may be due to the analysis process not running first or an issue with the AI model analyzing the raw data.")
         return
 
     # setup date range
@@ -54,21 +48,21 @@ def view_evaluation_heat_map_by_server(space_id, command):
     year = max_date.year
     days_in_month = monthrange(year, month)[1]
 
-    # prepare core dataframe (Asset/Date/Count)
+    # prepare core dataframe (PivotColumn/Date/Count)
     df['Date'] = pd.to_datetime(df[PIVOT_DATE_COLUMN]).dt.normalize()
     df = df[(df['Date'] >= f"{year}-{month}-1") & (df['Date'] <= f"{year}-{month}-{days_in_month}")]
-    df = df[['Asset', 'Date']]
-    df = df.groupby(['Asset', 'Date']).size().reset_index(name='Count')
+    df = df[[pivot_column, 'Date']]
+    df = df.groupby([pivot_column, 'Date']).size().reset_index(name='Count')
     logging.info("Refined Data Frame: %s", df.info())
 
     # append total count (Asset/Count)
-    df['Total'] = df.groupby('Asset')['Count'].transform('sum')
+    df['Total'] = df.groupby(pivot_column)['Count'].transform('sum')
 
     # sort data frame
-    df = df.sort_values(by=['Total', 'Asset', 'Date'], ascending=[False, True, True])
+    df = df.sort_values(by=['Total', pivot_column, 'Date'], ascending=[False, True, True])
 
     # build header
-    header = ["Config Item"]
+    header = [pivot_entity_type]
     for i in range(days_in_month):
         header.append(str(i + 1))
     header.append("Count")
@@ -78,13 +72,13 @@ def view_evaluation_heat_map_by_server(space_id, command):
     prior_row = None
     for index, row in df.iterrows():
         # collect data points
-        asset = row["Asset"]
+        pivot = row[pivot_column]
         date = row["Date"]
         date_count = row["Count"]
         total = row["Total"]
 
         # create new row?
-        if prior_row is not None and prior_row[0] != asset:
+        if prior_row is not None and prior_row[0] != pivot:
             prior_row = None
 
         # build new row
@@ -94,20 +88,40 @@ def view_evaluation_heat_map_by_server(space_id, command):
                 break
 
             # create new row
-            asset_row = [asset]
+            pivot_row = [pivot]
             for i in range(days_in_month):
                 cell_value = None
                 if date.day == i:
                     cell_value = date_count
-                asset_row.append(cell_value)
-            asset_row.append(total)
+                pivot_row.append(cell_value)
+            pivot_row.append(total)
 
-            rows.append(asset_row)
-            prior_row = asset_row
+            rows.append(pivot_row)
+            prior_row = pivot_row
         
         # update existing
         else:
             prior_row[date.day + 1] = date_count
 
     # display heatmap
-    show_heatmap_grid_widget(f"Top {MAX_ROWS} Incident Heat Map", year, month, header, rows)
+    show_heatmap_grid_widget(f"{title}", year, month, header, rows)
+
+
+def view_evaluation_heat_map_by_issue(space_id, command):
+    # retrieve data set
+    df = command.analysis_df
+    if df is None or len(df) == 0:
+        st.write("Please run the analysis tool first.")
+        return
+
+    render_heat_map("Classified Incident Heat Map", "Issue Type", "Category", df)
+
+
+def view_evaluation_heat_map_by_server(space_id, command):
+    # retrieve data set
+    df = command.analysis_df
+    if df is None or len(df) == 0:
+        st.write("Please run the analysis tool first.")
+        return
+
+    render_heat_map(f"Top {MAX_ROWS} Incident Heat Map", "Config Item", "Asset", df)
